21f56815c2930fe37c444f8ac39ddaf45acdd87d
Sergey Agarkov <grey1701@gmail.com>
2025-05-05 22:57:22 +0300

    fix images and mobile version

--- script/mobile-menu.js
+++ script/mobile-menu.js
@@ -3,17 +3,9 @@ const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
-const toggleBtnOpen = () => menuBtnOpen.classList.toggle('is-open');
-const toggleBtnClose = () => menuBtnClose.classList.toggle('is-open');
-const handleMenuClick = () => {
-  toggleMenu();
-  toggleBtnOpen();
-  toggleBtnClose();
-};
-const disableScroll = () => document.body.classList.toggle('is-scroll-disabled');
 
-menuBtnOpen.addEventListener('click', handleMenuClick);
-menuBtnClose.addEventListener('click', handleMenuClick);
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
 
 menuBtnOpen.addEventListener('click', disableScroll);
 menuBtnClose.addEventListener('click', disableScroll);
