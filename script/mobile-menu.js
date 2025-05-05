1a57bbe6495ce8ea24e5bbefa92cd3f0e3d04af5
Sergey Agarkov <grey1701@gmail.com>
2025-05-05 15:41:09 +0300

    fix adaptive for 360 and 480

--- script/mobile-menu.js
+++ script/mobile-menu.js
@@ -3,10 +3,17 @@ const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleBtnOpen = () => menuBtnOpen.classList.toggle('is-open');
+const toggleBtnClose = () => menuBtnClose.classList.toggle('is-open');
+const handleMenuClick = () => {
+  toggleMenu();
+  toggleBtnOpen();
+  toggleBtnClose();
+};
 const disableScroll = () => document.body.classList.toggle('is-scroll-disabled');
 
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+menuBtnOpen.addEventListener('click', handleMenuClick);
+menuBtnClose.addEventListener('click', handleMenuClick);
 
 menuBtnOpen.addEventListener('click', disableScroll);
 menuBtnClose.addEventListener('click', disableScroll);
